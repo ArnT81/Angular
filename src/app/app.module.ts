10d6e9dca594515150a4260265d83eae3aa38d4e
ArnT <anders_soderberg@hotmail.com>
2020-02-11 07:20:28 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersListItemComponent } from './users-list-item/users-list-item.component';
+import { EditUsersComponent } from './edit-users/edit-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UsersListComponent,
+    UsersListItemComponent,
+    EditUsersComponent
   ],
   imports: [
     BrowserModule,
