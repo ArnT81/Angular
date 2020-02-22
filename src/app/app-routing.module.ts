1d349d07847078679f12b3d830fcc51546aa2163
ArnT <anders_soderberg@hotmail.com>
2020-02-22 16:45:40 +0100

    navbar present

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SingleUserComponent } from './single-user/single-user.component';
 
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'User',
+    component: SingleUserComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
