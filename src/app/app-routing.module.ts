40d7fa9eb260190e3b2e1fe8e32054ca5fdf6deb
ArnT <anders_soderberg@hotmail.com>
2020-02-23 11:50:32 +0100

    routing working again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,11 +9,15 @@ import { SingleUserComponent } from './single-user/single-user.component';
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: Component
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   },
   {
     path: 'user',
