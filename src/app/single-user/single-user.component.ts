018d29538de4db69d1417ad895e5f90f886f5dd0
ArnT <anders_soderberg@hotmail.com>
2020-02-24 19:55:44 +0100

    prescribtion in singleuser component atleast logged

--- src/app/single-user/single-user.component.ts
+++ src/app/single-user/single-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-user',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    router.events.subscribe((value) => {
+      console.log(value); // value from event
+      return value
+    })
+  }
 
   ngOnInit() {
   }
